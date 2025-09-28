b09a4f6194622d563019508f06e7b75b3719f830
Tecito <A01567189@tec.mx>
2025-09-28 10:53:06 -0700

    Update UsuariosForm.js

---
+++ app/usuarios/UsuariosForm.js
@@ -0,0 +1,5 @@
+import 'expo-router/entry'
+import {useRouter} from 'expo-router';
+import React, { useState } from 'react';
+import {View, StyleSheet, StatusBar, SafeAreaView, ScrollView, Text, TouchableOpacity, Pressable, Image} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
