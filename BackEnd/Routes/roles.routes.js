ca8008608eafd7f52c37a7b9d856144cb796bac1
Luis Alejandro Carrillo Valdez <151704695+Carrillo-coder@users.noreply.github.com>
2025-10-14 19:30:47 -0600

    Revert "Diego terrazas usuarios"

--- BackEnd/Routes/roles.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const RolController = require('../Controllers/roles.controller');
-
-// GET - Obtener todos los roles
-router.get('/', RolController.getAll);
-
-module.exports = router;
