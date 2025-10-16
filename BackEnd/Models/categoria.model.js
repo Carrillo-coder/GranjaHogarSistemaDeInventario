1f423ed7ff80256d5a95fdbca215423b84880685
Héctor Alfonso <147531020+A01563545@users.noreply.github.com>
2025-10-15 21:41:41 -0600

    Revert "Revert "Carrillo endpoints integration""

--- BackEnd/Models/categoria.model.js
+++ BackEnd/Models/categoria.model.js
@@ -1,16 +1,25 @@
+/**
+ * Modelo de Categoria usando Sequelize
+ * @param {object} sequelize - Instancia de Sequelize
+ * @param {object} Sequelize - Constructor de Sequelize
+ */
 module.exports = (sequelize, Sequelize) => {
     const Categoria = sequelize.define("Categoria", {
         idCategoria: {
             type: Sequelize.INTEGER,
             autoIncrement: true,
-            primaryKey: true
+            primaryKey: true,
+            field: 'idCategoria'
         },
         Nombre: {
-            type: Sequelize.STRING,
-            allowNull: false
+            type: Sequelize.STRING(50),
+            allowNull: false,
+            unique: true,
+            field: 'Nombre'
         }
     }, {
-        tableName: 'categorias'
+        tableName: 'Categoria',
+        timestamps: false
     });
 
     return Categoria;
--- BackEnd/Models/index.js
+++ BackEnd/Models/index.js
@@ -1,17 +1,29 @@
-const { sequelize } = require('../config/db.config');
-const Sequelize = require('sequelize');
+const dbConfig = require("../config/db.config.js");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+    }
+});
 
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.Usuarios = require('./usuarios.model.js')(sequelize, Sequelize);
-db.Roles = require('./roles.model.js')(sequelize, Sequelize);
-db.TiposSalidas = require('./tiposSalidas.model.js')(sequelize, Sequelize);
-db.Categoria = require('./categoria.model.js')(sequelize, Sequelize);
+db.Usuario = require("./usuarios.model.js")(sequelize, Sequelize);
+db.Rol = require("./roles.model.js")(sequelize, Sequelize);
+db.TiposSalidas = require("./tiposSalidas.model.js")(sequelize, Sequelize);
+db.Categoria = require("./categoria.model.js")(sequelize, Sequelize);
 
-// Relationships
-// Example: db.Usuarios.belongsTo(db.Roles, { foreignKey: 'ID_Rol' });
+db.Rol.hasMany(db.Usuario, { foreignKey: 'idRol', as: 'usuarios' });
+db.Usuario.belongsTo(db.Rol, { foreignKey: 'idRol', as: 'rol' });
 
 module.exports = db;
