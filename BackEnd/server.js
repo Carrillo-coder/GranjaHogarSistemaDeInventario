cacce0c7e9cf9a7a04e03aaeafc240724c15879e
Luis Alejandro Carrillo Valdez <A01567228@tec.mx>
2025-10-19 15:45:06 -0600

    Patch 1 post integración, added missing , on package.json and deleted extra ) on server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -44,6 +44,5 @@ const startServer = async () => {
 process.on('unhandledRejection', (error) => {
     console.error('❌ Error no manejado:', error);
     process.exit(1);
-  }
-})();
+  })();
 startServer();
