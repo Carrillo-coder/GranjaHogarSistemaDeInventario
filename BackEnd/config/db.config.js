96e24575321f5f46595695a4dbc6e78c77b73986
TecitoWN89 <A01567189@tec.mx>
2025-10-20 22:24:55 -0700

    DiegoTerrazasIgnoreFix

---
+++ BackEnd/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",      // tu host, en XAMPP normalmente localhost
+    USER: "root",           // usuario de MySQL, por defecto "root"
+    PASSWORD: "tecito",           // contraseña de MySQL, por defecto vacío en XAMPP
+    DB: "inventario",      // nombre de tu base de datos
+    dialect: "mysql",       // porque usamos MySQL
+    pool: {
+        max: 5,             // conexiones máximas
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
