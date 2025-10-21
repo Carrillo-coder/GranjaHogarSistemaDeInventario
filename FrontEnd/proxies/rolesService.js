13b0434ee160f66770fae96bdab37ed23f45cb4a
TecitoWN89 <A01567189@tec.mx>
2025-10-20 21:46:57 -0700

    DiegoTeFixApi

--- FrontEnd/proxies/rolesService.js
+++ FrontEnd/proxies/rolesService.js
@@ -2,7 +2,7 @@ import { API_BASE_URL } from '@env'
 
 const RolesServiceProxy = () => { 
   const API_URL = API_BASE_URL || 'http://10.0.2.2:3000';
-  const baseUrl = `${API_URL}/roles`
+  const baseUrl = `${API_URL}/api/inventario/roles`
 
   async function getAllRoles() {
     const response = await fetch(baseUrl, {
