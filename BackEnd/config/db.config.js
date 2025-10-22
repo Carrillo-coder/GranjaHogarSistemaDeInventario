519024c3eaa9e5a3bbde324ee66e616233bc1e71
Rocio Rodriguez <a01563530@tec.mx>
2025-10-22 00:47:56 -0600

    Arreglo de las llamadas a los proxies

---
+++ BackEnd/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",      // tu host, en XAMPP normalmente localhost
+    USER: "root",           // usuario de MySQL, por defecto "root"
+    PASSWORD: "Elysareza1",           // contraseña de MySQL, por defecto vacío en XAMPP
+    DB: "granja-hogar",      // nombre de tu base de datos
+    dialect: "mysql",       // porque usamos MySQL
+    pool: {
+        max: 5,             // conexiones máximas
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
