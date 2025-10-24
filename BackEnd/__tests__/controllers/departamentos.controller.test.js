700c16966ebad404c325e9a656045e74ace68504
Stirk23 <a01563542@tec.mx>
2025-10-24 14:38:34 -0600

    Tests backend listos

--- BackEnd/__tests__/controllers/departamentos.controller.test.js
+++ BackEnd/__tests__/controllers/departamentos.controller.test.js
@@ -14,10 +14,29 @@ describe('Departamentos Controller Tests', () => {
 
     describe('getAll', () => {
         test('Given departamentos in database When getAll Then returns 200 and data', async () => {
+            DepartamentosService.getAllDepartamentos.mockResolvedValue({
+                statusCode: 200,
+                success: true,
+                data: [
+                    { idDepartamento: 1, nombre: 'Sistemas' },
+                    { idDepartamento: 2, nombre: 'Mecatrónica' }
+                ],
+            });
+
             const res = await request(app)
                 .get('/api/inventario/departamentos');
-            expect(res.status).toBe(200);
+            expect(res.statusCode).toBe(200);
             expect(res.body.success).toBe(true);
+        });
+
+        test('Given no departamentos in database When getAll Then returns 204', async () => {
+            DepartamentosService.getAllDepartamentos.mockResolvedValue({
+                statusCode: 204,
+                success: false,
+            });
+            const res = await request(app)
+                .get('/api/inventario/departamentos');
+            expect(res.statusCode).toBe(204);
         });
     });
 });
